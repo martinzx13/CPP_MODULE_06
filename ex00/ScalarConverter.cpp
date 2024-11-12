8b6f06a980724b1350653ab53b74eebc9beb614d
Juan Aldana <juan.martinez43@udea.edu.co>
2024-11-12 20:33:18 +0000

    (update) ex001

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,18 +1,57 @@
 #include "ScalarConverter.hpp"
-
-ScalarConverter::ScalarConverter(){}
-ScalarConverter::ScalarConverter(const ScalarConverter &_other){}
-
-ScalarConverter::~ScalarConverter(){}
-
-void stringToInt(const std::string &_value, int &_int)
-{
-
+#include <exception>
+#include <iostream>
+#include <limits.h>
+#include <limits>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+ScalarConverter::ScalarConverter() {}
+ScalarConverter::ScalarConverter(const ScalarConverter &_other) {
+  if (this != &_other)
+    return;
 }
-void ScalarConverter::convert(const std::string &_value)
-{
-    int numb;
 
-    stringToInt(_value, numb);
-    _int = numb;
+ScalarConverter::~ScalarConverter() {}
+
+bool stringToInt(const std::string &_value, int &_int) {
+
+  if (_value.size() == 1) {
+    _int = static_cast<char>(_value[0]);
+    throw(_value[0]);
+  }
+  std::stringstream oss(_value);
+
+  oss >> _int;
+
+  if (oss.fail() || !oss.eof())
+    return (false);
+  return (true);
+}
+
+bool stringToChar(const int &_value, char &_char) {
+
+  _char = static_cast<char>(_value);
+  if (_char)
+    return true;
+  return false;
+}
+void ScalarConverter::convert(const std::string &_value) {
+  int _int;
+  //   char _char;
+  //   float _float;
+  //   double _double;
+
+  try {
+
+    if (stringToInt(_value, _int))
+      std::cout << "int : " << _int << std::endl;
+    // : std::cout << "int : " << "Impossible Conversion 🗿" << std::endl;
+  } catch (int value) {
+    std::cerr << "Error ::: " << value << std::endl;
+  } catch (char const *err) {
+    std::cerr << "Error ::" << err << std::endl;
+  } catch (char const err) {
+    std::cerr << "Err : : : " << err << std::endl;
+  }
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -5,18 +5,14 @@
 
 class ScalarConverter {
 private:
-  int _int;
-  float _float;
-  double _double;
-
   ScalarConverter();
   ScalarConverter(const ScalarConverter &_other);
   ScalarConverter &operator=(const ScalarConverter &_other);
   ~ScalarConverter();
-
+  
 public:
   static void convert(const std::string &_value);
 };
 #endif
 
-void stringToInt(const std::string &value, int &_int);
+bool stringToInt(const std::string &value, int &_int);
