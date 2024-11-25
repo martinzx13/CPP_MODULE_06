722748c3512881318addeea6d09cc830c5c22087
martinzx13 <juan.martinxz43@gmail.com>
2024-11-25 18:18:36 +0000

    (update) the reference function

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -15,6 +15,13 @@
 #include <iostream>
 #include <stdint.h>
 
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+#define BLUE "\033[34m"
+#define YELLOW "\033[33m"
+#define MAGENTA "\033[35m"
+
 typedef struct {
   int data;
 } Data;
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,9 +5,14 @@ int main()
     Data data;
     data.data = 42;
     uintptr_t serialized = Serializer::serialize(&data);
+    uintptr_t serialized2 = uintptr_t(&data);
+
     Data *deserialized = Serializer::deserialize(serialized);
-    if (deserialized == &data)
+    if (deserialized == &data && serialized == serialized2)
     {
+        std::cout << GREEN  << "Success" << std::endl;
+        std::cout << YELLOW << "Data: Original  : " << &data << std::endl;
+        std::cout << BLUE   << "Data Serialized : " << deserialized << RESET << std::endl;
         return 0;
     }
     return 1;
