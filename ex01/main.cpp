db8be9f79764b27c02d0f969f48656617fe01c41
Juan Aldana <juan.martinez43@udea.edu.co>
2024-12-02 12:15:16 +0000

    (update) the main file to add and extra case

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,16 +4,32 @@ int main()
 {
     Data data;
     data.data = 42;
+
+    Data data1;
+    data1.data = 24;
+
+    uintptr_t serialized1 = Serializer::serialize(&data1);
     uintptr_t serialized = Serializer::serialize(&data);
     uintptr_t serialized2 = uintptr_t(&data);
 
     Data *deserialized = Serializer::deserialize(serialized);
+    Data *deserialized1 = Serializer::deserialize(serialized1);
+
     if (deserialized == &data && serialized == serialized2)
     {
         std::cout << GREEN  << "Success" << std::endl;
         std::cout << YELLOW << "Data: Original  : " << &data << std::endl;
         std::cout << BLUE   << "Data Serialized : " << deserialized << RESET << std::endl;
-        return 0;
+    }
+
+    if (deserialized1 == &data1 && serialized1 == serialized2)
+    {
+        std::cout << GREEN  << "Success" << std::endl;
+        std::cout << YELLOW << "Data: Original  : " << &data << std::endl;
+        std::cout << BLUE   << "Data Serialized : " << deserialized << RESET << std::endl;
+    }
+    else {
+        std::cout << RED << "Error data are not the same is different" << std::endl;
     }
     return 1;
 }
